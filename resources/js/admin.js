ec67d4ee4cc6eca28ac36e12fee6d3eb2891cf73
Vitaliy_il <ilinov.vitalii@pdffiller.team>
2021-01-21 19:08:09 +0200

    add admin-lte

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,4 +1,7 @@
 require('./bootstrap');
+require('admin-lte')
 require('./codeMirror');
 require('./tables');
 window.Sortable = require('sortablejs').Sortable;
+window.toastr = require('toastr');
+window.hljs = require('highlight.js');
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,9 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-} catch (e) {}
+} catch (e) {
+    console.log(e);
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -23,9 +25,6 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-window.toastr = require('toastr');
-
-window.hljs = require('highlight.js');
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
