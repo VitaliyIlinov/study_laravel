e7eee41d5f40aa8820941d68ab450c4b97496efe
VitaliyIlinov <ilinov123@ukr.net>
2022-01-23 18:29:24 +0200

    github action  codesniffer test

--- public/github_test.php
+++ public/github_test.php
@@ -1,67 +1,100 @@
 <?php
-//todo
-// 1. permission for console command in study container,example:
-//      container php artisan make:model T
-//      app create ass www-data but in console as root
-// 2. permission for mysql container
-// 3. docker/configs/mysql/conf.d/my.cnf permission denied if add path  to log
-//https://www.youtube.com/watch?v=cQWeTnOwlm4
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+namespace App\Http\Controllers\Admin;
 
-define('LARAVEL_START', microtime(true));
+use App\Http\Controllers\Admin\Traits\CrudService;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TodoRequest;
+use App\Models\TodoList;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
+class TodoListController extends Controller
+{
+    use CrudService {
+        CrudService::index as crudIndex;
+        CrudService::show as crudShow;
+        CrudService::create as crudCreate;
+        CrudService::destroy as crudDelete;
+        CrudService::update as crudUpdate;
+        CrudService::store as crudStore;
+    }
 
-require __DIR__.'/../vendor/autoload.php';
+    protected const IS_CRUD_BY_AJAX = true;
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+    protected function fields(): array
+    {
+        return [
+            'id'          => [
+                'trans' => 'Id',
+            ],
+            'description' => [
+                'trans' => 'description',
+                'type'  => 'textarea',
+            ],
+            'label'       => [
+                'trans' => 'label',
+                'type'  => 'text',
+            ],
+            'status'      => [
+                'trans' => 'status',
+                'type'  => 'checkbox',
+            ],
+            'sort'        => [
+                'trans' => 'sort',
+                'type'  => 'text',
+            ],
+            'updated_at'  => [
+                'trans' => 'updated_at',
+            ],
+        ];
+    }
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+    public function index()
+    {
+        return $this->crudIndex(
+            TodoList::all()->sortBy('sort'), 'Todo List',   'admin.todo.todo');
+    }
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+    public function create(Request $request)
+    {
+        return $this->crudCreate($request);
+    }
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    public function show(TodoList $todoList, Request $request)
+    {
+        return $this->crudShow($todoList, $request);
+    }
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+    public function store(TodoRequest $request)
+    {
+        return $this->crudUpdate($this->mergeStatus($request), new TodoList(), 'todo_list');
+    }
 
-$response->send();
+    public function update(TodoRequest $request, TodoList $todoList)
+    {
+        return $this->crudUpdate($this->mergeStatus($request), $todoList, 'todo_list');
+    }
 
-$kernel->terminate($request, $response);
+    public function destroy(TodoList $todoList): JsonResponse
+    {
+        return $this->crudDelete($todoList);
+    }
+
+    public function ajaxUpdateSort(Request $request): JsonResponse
+    {
+        $lists = explode('|', $request->get('list'));
+        $i = 0;
+        foreach ($lists as $id) {
+            TodoList::findOrFail($id)->update(['sort' => ++$i]);
+        }
+        return response()->json('success');
+    }
+
+    public function changeStatus(TodoList $todoList, Request $request): JsonResponse
+    {
+        $todoList->status = $request->get('status');
+        $todoList->save();
+        return response()->json('success');
+    }
+}
